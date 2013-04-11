182b7dfeb9c7ddd8a3e0a92656f4d936f4637041
David Espinal Ruiz <davidesr7@gmail.com>
2013-04-11 11:27:40 -0500

    inicio
    inicio

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
-ksjdksdksjkasasasasa1
-asalskalks
-bbbbb
-desde mi casa
+var express = require('express');
+var app     = express();
+var server  = require('http').createServer(app);
+var io      = require('socket.io').listen(server);
+var swig    = require('swig');
+var cons    = require('consolidate');
+
+var users = {}
+
+swig.init({
+	cache : false
+});
+
+app.configure(function(){
+	app.use(express.bodyParser());
+	app.use( express.cookieParser() );
+});
+
+app.engine('.html', cons.swig);
+app.set('view engine', 'html');
+app.use(express.static('./public'));
+
+app.get('/', function (req, res) {
+	res.render('index', {
+		titulo : 'Ejemplo'
+	});
+});
+
+server.listen(3000);
