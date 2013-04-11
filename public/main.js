92131b11a27e35d54d925b1c22d29ab628a009d0
David Espinal Ruiz <davidesr7@gmail.com>
2013-04-11 17:03:05 -0500

    algo

--- public/main.js
+++ public/main.js
@@ -2,17 +2,39 @@ $(document).on("ready", inicializarEventos);
 var socket;
 
 function inicializarEventos() {
-	socket = io.connect('http://localhost:3000');
-	socket.on('respuestaServer', function (data) {
-		$("#mensajes").val(data);
-	});
-	
-    var x;
-    x = $("#enviarMensaje");
-    x.click(presionParrafo);
-}
 
-function presionParrafo() {
-	socket.emit('eventoEnviarMensaje', $("#message").val());
+	socket = io.connect('http://localhost:3000');
+
+	//Conectar
+    $("#conectar").click(function() {
+    	socket.emit("nick", $("#nick").val());
+    });
+
+    //Recibe Usuarios Conectados
+	socket.on('nicks', function (nicks) {
+		$("#usuarios").empty();
+		for (var i = 0; i < nicks.length; i++) {
+			$("#usuarios").append('<option value="' + nicks[i].idSocket + '">' + nicks[i].userName + '</option>');
+      	}
+	});
+
+	//Enviar Mensaje Publico al Server
+    $("#enviarMensajePublico").click(function() {
+		socket.emit('eventoEnviarMensajePublico', $("#message").val());
+	});
+
+	//Enviar Mensaje Privado al Server
+    $("#enviarMensajePrivado").click(function() {
+    	var selectedUser = $('#usuarios :selected').val();
+		socket.emit('eventoEnviarMensajePrivado', {
+			message: $("#message").val(),
+			idSocket: selectedUser
+		});
+	});
+
+	//Recibe Mensaje del Server
+	socket.on('respuestaServer', function (data) {
+		$("#mensajes").append('<option>' + data + '</option>');
+	});
 }
 
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var io      = require('socket.io').listen(server);
 var swig    = require('swig');
 var cons    = require('consolidate');
 
-var users = {}
+var users = []
 
 swig.init({
 	cache : false
@@ -27,8 +27,34 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-	socket.on('eventoEnviarMensaje', function (data) {
-		socket.emit('respuestaServer', data + " --> Server");
+
+	//Envia Todos los Usuarios Conectados
+	socket.emit('nicks', users);
+
+	//Mensaje Público: Recibe Mensaje del Cliente y lo Envia a todos los Sockets
+	socket.on('eventoEnviarMensajePublico', function (data) {
+		/*socket.emit('respuestaServer', data);
+		socket.broadcast.emit('respuestaServer', data);*/
+		io.sockets.emit('respuestaServer', data);
+	});
+
+	//Mensaje Privado: Recibe Mensaje del Cliente y lo Envia a un Socket Destino
+	socket.on('eventoEnviarMensajePrivado', function (data) {
+		io.sockets.socket(data.idSocket).emit('respuestaServer', data.message);
+		//io.sockets.socket(targetSocket[1]).broadcast.emit('respuestaServer', targetSocket[0]);
+		/*io.sockets.clients().forEach(function (eachSocket) {
+			if(eachSocket.store.id == targetSocket[1]) {
+				eachSocket.emit('respuestaServer', targetSocket[0]);
+			}
+		});*/
+	});
+
+	
+	//Recibe Conexión del Cliente
+	socket.on('nick', function (user) {
+		var newUser = { userName: user, idSocket : socket.id};
+		users.push(newUser);
+		socket.emit('nicks', users);
 	});
 });
 
